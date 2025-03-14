cbf9d6fbfd08deec2ca685f671ef9ba32ff77091
dzee <df.zee@hotmail.com>
2025-03-14 17:12:08 +0100

    tn and er

--- DQNAgent.py
+++ DQNAgent.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from collections import deque
+import random
 
 
 class NeuralNet(nn.Module):
@@ -21,18 +23,34 @@ class NeuralNet(nn.Module):
 
 
 class DQNAgent:
-    def __init__(self, n_actions, n_states, alpha, gamma, update_freq, hidden_dim):
+    def __init__(self, n_actions, n_states, alpha, gamma, update_freq, hidden_dim, tn=False, er=False):
         self.n_actions = n_actions
         self.n_states = n_states
         self.gamma = gamma
         self.update_freq = update_freq
+
+        self.tn = tn
+        self.er = er
+        self.memory_size = 10000
+        self.batch_size = 64
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         # self.device = torch.device("cpu")
         self.loss_function = nn.MSELoss().to(self.device)
         self.Q = NeuralNet(n_states, n_actions, self.device, hidden_dim)
         self.optimizer = optim.Adam(self.Q.parameters(), lr=alpha)
-        self.update_buffer = []
+        self.update_count = 0
+
+        if tn:
+            self.Q_target = NeuralNet(n_states, n_actions, self.device, hidden_dim)
+            self.Q_target.load_state_dict(self.Q.state_dict())
+            self.Q_target.eval()
+        if er:
+            # use the replay buffer to sample from every update
+            self.replay_buffer = deque(maxlen=self.memory_size)
+        else:
+            # collect the steps up to the next update
+            self.update_buffer = []
 
     def select_action(self, state, epsilon):  # ϵ-greedy policy
         if np.random.random() > epsilon:
@@ -45,12 +63,24 @@ class DQNAgent:
         return action
 
     def update(self, state, action, reward, next_state, done):  # Q-Learning update equation
-        self.update_buffer.append((state, action, reward, next_state, done))
-        if len(self.update_buffer) < self.update_freq:
+        self.update_count += 1
+        if self.er:
+            self.replay_buffer.append((state, action, reward, next_state, done))
+            if len(self.replay_buffer) < self.batch_size:
+                return
+        else:
+            self.update_buffer.append((state, action, reward, next_state, done))
+        if self.update_count < self.update_freq:
             return
 
-        state, action, reward, next_state, done = (np.array(x) for x in zip(*self.update_buffer))
-        self.update_buffer.clear()
+        self.update_count = 0
+
+        if self.er:
+            batch = random.sample(self.replay_buffer, self.batch_size)
+            state, action, reward, next_state, done = (np.array(x) for x in zip(*batch))
+        else:
+            state, action, reward, next_state, done = (np.array(x) for x in zip(*self.update_buffer))
+            self.update_buffer.clear()
 
         state = torch.tensor(state, dtype=torch.float, device=self.device)
         action = torch.tensor(action, dtype=torch.int64, device=self.device).unsqueeze(1)
@@ -61,7 +91,11 @@ class DQNAgent:
         q_value = self.Q.forward(state).gather(1, action).squeeze(1)
 
         with torch.no_grad():
-            q_next = self.Q.forward(next_state).max(1)[0]
+            if self.tn:
+                q_next = self.Q_target.forward(next_state).max(1)[0]
+            else:
+                q_next = self.Q.forward(next_state).max(1)[0]
+
         q_target = reward + self.gamma * q_next * (1 - done)
 
         q_loss = self.loss_function(q_value, q_target)
@@ -69,3 +103,7 @@ class DQNAgent:
         self.optimizer.zero_grad()
         q_loss.backward()
         self.optimizer.step()
+
+    def update_tn(self):
+        if self.tn:
+            self.Q_target.load_state_dict(self.Q.state_dict())
--- DQNExperiment.py
+++ DQNExperiment.py
@@ -21,13 +21,16 @@ def evaluation(agent: DQNAgent):
 
 
 def run_single_repetition(args):
-    rep_id, n_envsteps, eval_internal, epsilon, decay_rate, alpha, gamma, update_freq, hidden_dim = args
+    rep_id, n_envsteps, eval_internal, epsilon, decay_rate, alpha, gamma, update_freq, hidden_dim, tn, er = args
+    tn_update_freq = 1000
+
     # Create a new environment and agent for each repetition.
     env = gym.make('CartPole-v1')
     n_actions = env.action_space.n
     n_states = env.observation_space.shape[0]
     eval_returns = np.zeros(int(n_envsteps / eval_internal))
-    agent = DQNAgent(n_actions, n_states, alpha, gamma, update_freq, hidden_dim)
+    eval_epsilon = np.zeros(int(n_envsteps / eval_internal))
+    agent = DQNAgent(n_actions, n_states, alpha, gamma, update_freq, hidden_dim, tn, er)
 
     eval_num = 0
     s, info = env.reset()
@@ -37,6 +40,9 @@ def run_single_repetition(args):
         agent.update(s, a, r, s_next, done)
         s = s_next
 
+        if tn and step % tn_update_freq == 0:
+            agent.update_tn()
+
         if done or trunc:
             s, info = env.reset()
             epsilon *= decay_rate
@@ -44,29 +50,33 @@ def run_single_repetition(args):
         if step % eval_internal == 0:
             eval_return = evaluation(agent)
             eval_returns[eval_num] = eval_return
+            eval_epsilon[eval_num] = epsilon
             eval_num += 1
 
             print(f"Running repitition {rep_id+1:2}, Environment steps: {step:6}, "
                   f"Epsilon: {epsilon:7}, Eval return: {eval_return:3}")
 
-    return eval_returns
+    return eval_returns, eval_epsilon
 
 
 def run_repetitions_multiprocessing(outfile, n_processes, n_repetitions, n_envsteps, eval_interval, epsilon, decay_rate,
-                                    alpha, gamma, update_freq, hidden_dim):
+                                    alpha, gamma, update_freq, hidden_dim, tn, er):
     print("Running repetitions with the following settings:")
     print(locals())
 
     # Create a list of argument tuples for each repetition.
-    args_list = [(rep, n_envsteps, eval_interval, epsilon, decay_rate, alpha, gamma, update_freq, hidden_dim)
+    args_list = [(rep, n_envsteps, eval_interval, epsilon, decay_rate, alpha, gamma, update_freq, hidden_dim, tn, er)
                  for rep in range(n_repetitions)]
 
     # Use a multiprocessing Pool to run each repetition in parallel.
     with mp.Pool(processes=n_processes) as pool:
         results = pool.map(run_single_repetition, args_list)
 
-    eval_returns = np.array(results)
+    eval_returns, eval_epsilon = zip(*results)
+    eval_returns = np.array(eval_returns)
+    eval_epsilon = np.array(eval_epsilon)
     np.savetxt(outfile, eval_returns, delimiter=",")
+    np.savetxt(outfile.strip(".csv") + "_eps.csv", eval_epsilon, delimiter=",")
 
 
 def experiment_1(n_repetitions, n_envsteps, eval_interval):
@@ -74,7 +84,9 @@ def experiment_1(n_repetitions, n_envsteps, eval_interval):
     epsilon = 1
     decay_rate = 0.999
     update_freq = 4
-    hidden_dim = 128
+    hidden_dim = 64
+    tn = False
+    er = False
 
     learning_rates = [0.0001, 0.001, 0.01]
 
@@ -87,10 +99,11 @@ def experiment_1(n_repetitions, n_envsteps, eval_interval):
     n_processes = 3  # set the number of processes for parallel execution
 
     for alpha in learning_rates:
-        outfile = os.path.join(outdir, f"a_{alpha}_uf_{update_freq}_dr_{decay_rate}_hd_{hidden_dim}.csv")
+        outfile = os.path.join(
+            outdir, f"a_{alpha}_uf_{update_freq}_dr_{decay_rate}_hd_{hidden_dim}_tn_{tn}_er_{er}.csv")
         if not os.path.exists(outfile):
             run_repetitions_multiprocessing(outfile, n_processes, n_repetitions, n_envsteps, eval_interval, epsilon,
-                                            decay_rate, alpha, gamma, update_freq, hidden_dim)
+                                            decay_rate, alpha, gamma, update_freq, hidden_dim, tn, er)
         eval_returns = np.loadtxt(outfile, delimiter=",", ndmin=2)
         mean_eval_returns = np.mean(eval_returns, axis=0)
         conf_eval_returns = np.std(eval_returns, axis=0) / np.sqrt(n_repetitions)
@@ -107,7 +120,9 @@ def experiment_2(n_repetitions, n_envsteps, eval_interval):
     epsilon = 1
     decay_rate = 0.999
     alpha = 0.001
-    hidden_dim = 128
+    hidden_dim = 64
+    tn = False
+    er = False
 
     update_freqs = [4, 32, 128]
 
@@ -120,10 +135,11 @@ def experiment_2(n_repetitions, n_envsteps, eval_interval):
     n_processes = 3  # set the number of processes for parallel execution
 
     for update_freq in update_freqs:
-        outfile = os.path.join(outdir, f"a_{alpha}_uf_{update_freq}_dr_{decay_rate}_hd_{hidden_dim}.csv")
+        outfile = os.path.join(
+            outdir, f"a_{alpha}_uf_{update_freq}_dr_{decay_rate}_hd_{hidden_dim}_tn_{tn}_er_{er}.csv")
         if not os.path.exists(outfile):
             run_repetitions_multiprocessing(outfile, n_processes, n_repetitions, n_envsteps, eval_interval, epsilon,
-                                            decay_rate, alpha, gamma, update_freq, hidden_dim)
+                                            decay_rate, alpha, gamma, update_freq, hidden_dim, tn, er)
         eval_returns = np.loadtxt(outfile, delimiter=",", ndmin=2)
         mean_eval_returns = np.mean(eval_returns, axis=0)
         conf_eval_returns = np.std(eval_returns, axis=0) / np.sqrt(n_repetitions)
@@ -140,7 +156,9 @@ def experiment_3(n_repetitions, n_envsteps, eval_interval):
     epsilon = 1
     alpha = 0.001
     update_freq = 4
-    hidden_dim = 128
+    hidden_dim = 64
+    tn = False
+    er = False
 
     decay_rates = [0.9999, 0.999, 0.99]
 
@@ -153,10 +171,11 @@ def experiment_3(n_repetitions, n_envsteps, eval_interval):
     n_processes = 3  # set the number of processes for parallel execution
 
     for decay_rate in decay_rates:
-        outfile = os.path.join(outdir, f"a_{alpha}_uf_{update_freq}_dr_{decay_rate}_hd_{hidden_dim}.csv")
+        outfile = os.path.join(
+            outdir, f"a_{alpha}_uf_{update_freq}_dr_{decay_rate}_hd_{hidden_dim}_tn_{tn}_er_{er}.csv")
         if not os.path.exists(outfile):
             run_repetitions_multiprocessing(outfile, n_processes, n_repetitions, n_envsteps, eval_interval, epsilon,
-                                            decay_rate, alpha, gamma, update_freq, hidden_dim)
+                                            decay_rate, alpha, gamma, update_freq, hidden_dim, tn, er)
         eval_returns = np.loadtxt(outfile, delimiter=",", ndmin=2)
         mean_eval_returns = np.mean(eval_returns, axis=0)
         conf_eval_returns = np.std(eval_returns, axis=0) / np.sqrt(n_repetitions)
@@ -168,6 +187,79 @@ def experiment_3(n_repetitions, n_envsteps, eval_interval):
     plot.save(name="naive_dqn_learning_curve_epsilon_decay")
 
 
+def experiment_4(n_repetitions, n_envsteps, eval_interval):
+    gamma = 1
+    epsilon = 1
+    decay_rate = 0.9999
+    alpha = 0.001
+    update_freq = 4
+    tn = False
+    er = False
+
+    hidden_dims = [16, 32, 64]
+
+    smoothing_window = 31
+    plot = LearningCurvePlot(f"Naive DQN learning curve (α = {alpha}, update freq = {update_freq})")
+
+    outdir = "evaluations"
+    os.makedirs(outdir, exist_ok=True)
+
+    n_processes = 3  # set the number of processes for parallel execution
+
+    for hidden_dim in hidden_dims:
+        outfile = os.path.join(
+            outdir, f"a_{alpha}_uf_{update_freq}_dr_{decay_rate}_hd_{hidden_dim}_tn_{tn}_er_{er}.csv")
+        if not os.path.exists(outfile):
+            run_repetitions_multiprocessing(outfile, n_processes, n_repetitions, n_envsteps, eval_interval, epsilon,
+                                            decay_rate, alpha, gamma, update_freq, hidden_dim, tn, er)
+        eval_returns = np.loadtxt(outfile, delimiter=",", ndmin=2)
+        mean_eval_returns = np.mean(eval_returns, axis=0)
+        conf_eval_returns = np.std(eval_returns, axis=0) / np.sqrt(n_repetitions)
+
+        plot.add_curve(range(eval_interval, n_envsteps+eval_interval, eval_interval), smooth(mean_eval_returns,
+                       window=smoothing_window), smooth(conf_eval_returns, window=smoothing_window),
+                       label=f"hidden dim = {hidden_dim}")
+
+    plot.save(name="naive_dqn_learning_curve_hidden_dim")
+
+
+def experiment_5(n_repetitions, n_envsteps, eval_interval):
+    gamma = 1
+    epsilon = 1
+    decay_rate = 0.999
+    alpha = 0.001
+    update_freq = 4
+    hidden_dim = 64
+
+    tns = [True, False]
+    ers = [True, False]
+
+    smoothing_window = 31
+    plot = LearningCurvePlot(f"Full DQN learning curve (α = {alpha}, update freq = {update_freq})")
+
+    outdir = "evaluations"
+    os.makedirs(outdir, exist_ok=True)
+
+    n_processes = 3  # set the number of processes for parallel execution
+
+    for tn in tns:
+        for er in ers:
+            outfile = os.path.join(
+                outdir, f"a_{alpha}_uf_{update_freq}_dr_{decay_rate}_hd_{hidden_dim}_tn_{tn}_er_{er}.csv")
+            if not os.path.exists(outfile):
+                run_repetitions_multiprocessing(outfile, n_processes, n_repetitions, n_envsteps, eval_interval, epsilon,
+                                                decay_rate, alpha, gamma, update_freq, hidden_dim, tn, er)
+            eval_returns = np.loadtxt(outfile, delimiter=",", ndmin=2)
+            mean_eval_returns = np.mean(eval_returns, axis=0)
+            conf_eval_returns = np.std(eval_returns, axis=0) / np.sqrt(n_repetitions)
+
+            plot.add_curve(range(eval_interval, n_envsteps+eval_interval, eval_interval), smooth(mean_eval_returns,
+                           window=smoothing_window), smooth(conf_eval_returns, window=smoothing_window),
+                           label=f"tn = {tn}, er = {er}")
+
+    plot.save(name="naive_dqn_learning_curve_tn_er")
+
+
 if __name__ == '__main__':
     n_repetitions = 5
     n_envsteps = 500000
@@ -176,3 +268,5 @@ if __name__ == '__main__':
     experiment_1(n_repetitions, n_envsteps, eval_interval)
     experiment_2(n_repetitions, n_envsteps, eval_interval)
     experiment_3(n_repetitions, n_envsteps, eval_interval)
+    experiment_4(n_repetitions, n_envsteps, eval_interval)
+    experiment_5(n_repetitions, n_envsteps, eval_interval)
